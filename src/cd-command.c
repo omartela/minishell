6138fe49385a84bc85217284bce21fd08aaa3e1c
omartela <oskari@gmail.com>
2024-09-05 10:42:36 +0300

    progressing...

--- src/cd-command.c
+++ src/cd-command.c
@@ -6,20 +6,54 @@
 /*   By: omartela <omartela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 16:06:06 by omartela          #+#    #+#             */
-/*   Updated: 2024/09/03 16:11:01 by omartela         ###   ########.fr       */
+/*   Updated: 2024/09/05 10:28:42 by omartela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../include/minishell.h"
 
-int	cd_command(char *str)
+char *get_env_value(char **envs, char *key)
+{
+	size_t	i;
+	size_t	len;
+
+	i = 0;
+	len = ft_strlen(key);
+	while (envs[i])
+	{
+		if (ft_strncmp(envs[i], key, len))
+	}
+}
+
+char *get_env_key(char **envs, char *value)
+{
+	size_t	i;
+	size_t	len;
+
+	i = 0;
+	len = ft_strlen(value);
+	while (envs[i])
+	{
+
+	}
+}
+
+int	cd_command(char *str, char **envs)
 {
 	if (str == NULL)
 	{
-		// Return to home directory.. when null;
+		if (chdir(""))
+			printf("Error when changing directory");
 	}
 	else
 	{
 		if (chdir(str))
 			printf("Error when changing directory");
 	}
+}
+
+int	main(int argc, char *argv[], char **envs)
+{
+	(void)argc;
+	(void)argv;
+	cd_command("../");
 }
