0217d210bed50cee83c8ec0e1b621e54366f0290
Iurii Rychkov <rychkoviurii@hotmail.com>
2024-09-15 17:03:23 +0300

    Added check_permissions for cmd. Covered error handling

--- src/exec_cmd.c
+++ src/exec_cmd.c
@@ -6,11 +6,43 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 17:58:11 by irychkov          #+#    #+#             */
-/*   Updated: 2024/09/15 13:11:34 by irychkov         ###   ########.fr       */
+/*   Updated: 2024/09/15 16:59:30 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static void	check_permissions(t_cmd *cmd, int is_abs_relative)
+{
+	int 	fd_test;
+
+	fd_test = -1;
+	if (is_abs_relative)
+	{
+		fd_test = open(cmd->args[0], O_DIRECTORY);
+		if (fd_test != -1)
+		{
+			close(fd_test);
+			show_error_free_cmd(126, cmd->args[0], "Is a directory", cmd);
+		}
+	}
+	if ((access(cmd->args[0], F_OK) == -1) && is_abs_relative)
+	{
+		show_error_free_cmd(127, cmd->args[0], "No such file or directory", cmd);
+		exit(127);
+	}
+	if ((access(cmd->args[0], X_OK) == -1) && is_abs_relative)
+	{
+		show_error_free_cmd(126, cmd->args[0], "Permission denied", cmd);
+		exit(126);
+	}
+	if (!is_abs_relative)
+	{
+		show_error_free_cmd(127, cmd->args[0], "command not found", cmd);
+		exit(127);
+	}
+	return ;
+}
 
 void	execute_command(t_cmd *cmd, char **envp)
 {
@@ -22,6 +54,7 @@ void	execute_command(t_cmd *cmd, char **envp)
 	if (cmd->args[0][0] == '/' || cmd->args[0][0] == '.')
 	{
 		execve(cmd->args[0], cmd->args, envp); //check if it exists
+		check_permissions(cmd , 1);
 		free_cmd(cmd);
 		error_sys("execve failed\n");
 		exit(1);
@@ -49,6 +82,10 @@ void	execute_command(t_cmd *cmd, char **envp)
 		free(full_path);
 		i++;
 	}
-	show_error_free_cmd(127, cmd->args[0], "command not found\n", cmd);
+/* 	show_error_free_cmd(127, cmd->args[0], "command not found\n", cmd); */
 	/* perror("execve failed"); */
+	check_permissions(cmd, 0);
+	free_cmd(cmd);
+	error_sys("execve failed\n");
+	exit(1);
 }
