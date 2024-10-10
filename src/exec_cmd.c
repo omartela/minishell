8b83ce1d586804b9a3f05320e23a8dfe01b70d23
omartela <Oskari Martela>
2024-10-10 20:30:01 +0300

    Fixes one unused variable cur path

--- src/exec_cmd.c
+++ src/exec_cmd.c
@@ -6,7 +6,7 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 17:58:11 by irychkov          #+#    #+#             */
-/*   Updated: 2024/10/10 18:12:50 by irychkov         ###   ########.fr       */
+/*   Updated: 2024/10/10 20:26:27 by omartela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,17 +75,14 @@ static void	execute_absolute_relative_command(t_cmd *cmd, char **envp)
 void	execute_command(t_cmd *cmd, char **envp)
 {
 	int		i;
-	char	*cur_path;
 	char	*full_path;
 
 	i = 0;
 	full_path = NULL;
-	cur_path = NULL;
 	if (cmd->args[0][0] == '/' || cmd->args[0][0] == '.')
 		execute_absolute_relative_command(cmd, envp);
 	if (!cmd->path)
 	{
-		cur_path = ft_strjoin("./", cmd->args[0]);
 		execve(cmd->args[0], cmd->args, envp);
 		check_permissions(cmd, 1);
 		show_error_free_cmd_exit(1, cmd->args[0], "command not found", cmd);
