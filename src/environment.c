0a54572fd44dede039474fa797f5d343dfff1996
omartela <Oskari Martela>
2024-10-11 11:41:36 +0300

    Updates append_table function to use change_table_value

--- src/environment.c
+++ src/environment.c
@@ -140,7 +140,6 @@ int	append_table(char ***table, const char *variable, const char *value)
 	size_t	size;
 	int		index_to_modify;
 	int		found;
-	char	*temp;
 
 	found = 0;
 	size = 0;
@@ -156,17 +155,13 @@ int	append_table(char ***table, const char *variable, const char *value)
 	}
 	if (found)
 	{
-		temp = (*table)[index_to_modify];
-		if (!ft_strchr(temp, '='))
+		if (!ft_strchr((*table)[index_to_modify], '='))
 		{
-			(*table)[index_to_modify] = ft_strjoin((*table)[index_to_modify], "=");
-			if (!(*table)[index_to_modify])
-				return (0);
+			if (change_table_value(table, index_to_modify, "="))
+				return (1);
 		}
-		(*table)[index_to_modify] = ft_strjoin((*table)[index_to_modify], value);
-		if (!(*table)[index_to_modify])
-			return (0);
-		free(temp);
+		if (change_table_value(table, index_to_modify, value))
+			return (1);
 		return (0);
 	}
 	else
