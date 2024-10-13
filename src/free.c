867a22edb6d6655140a2e7e77f7bf124e5553692
Rychkov Iurii <rychkoviurii@hotmail.com>
2024-10-13 14:44:12 +0300

    Set to NULL after free

--- src/free.c
+++ src/free.c
@@ -6,7 +6,7 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 15:50:09 by irychkov          #+#    #+#             */
-/*   Updated: 2024/10/11 17:51:54 by irychkov         ###   ########.fr       */
+/*   Updated: 2024/10/13 14:33:42 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,30 @@ void	free_array(char **array)
 	int	i;
 
 	i = 0;
+	/* if (!array)
+		return ; */
 	while (array[i])
 	{
 		free(array[i]);
+		array[i] = NULL;
 		i++;
 	}
 	free(array);
+	array = NULL;
 }
 
 void	free_array_back(char **array, size_t i)
 {
+	/* if (!array)
+		return ; */
 	while (i > 0)
 	{
 		i--;
 		free(array[i]);
+		array[i] = NULL;
 	}
 	free(array);
+	array = NULL;
 }
 
 void	free_cmd(t_cmd *cmd)
@@ -46,11 +54,20 @@ void	free_cmd(t_cmd *cmd)
 	if (cmd->path)
 		free_array(cmd->path);
 	if (cmd->infile)
+	{
 		free(cmd->infile);
+		cmd->infile = NULL;
+	}
 	if (cmd->outfile)
+	{
 		free(cmd->outfile);
+		cmd->outfile = NULL;
+	}
 	if (cmd->fd_heredoc)
+	{
 		free(cmd->fd_heredoc);
+		cmd->fd_heredoc = NULL;
+	}
 	free(cmd);
 	cmd = NULL;
 }
@@ -63,10 +80,13 @@ void	free_pipes(t_shell *sh)
 	while (i < sh->num_cmds - 1)
 	{
 		free(sh->pipes->fd[i]);
+		sh->pipes->fd[i] = NULL;
 		i++;
 	}
 	free(sh->pipes->fd);
+	sh->pipes->fd = NULL;
 	free(sh->pipes->pid);
+	sh->pipes->pid = NULL;
 	free(sh->pipes);
 	sh->pipes = NULL;
 }
@@ -76,15 +96,24 @@ void	free_shell(t_shell *sh)
 	if (sh->commands)
 		free_array(sh->commands);
 	if (sh->homepath)
+	{
 		free(sh->homepath);
+		sh->homepath = NULL;
+	}
 	if (sh->envp)
 		free_array(sh->envp);
 	if (sh->local_shellvars)
 		free_array(sh->local_shellvars);
 	if (sh->hd->heredoc_fds)
+	{
 		free(sh->hd->heredoc_fds);
+		sh->hd->heredoc_fds = NULL;
+	}
 	if (sh->hd)
+	{
 		free(sh->hd);
+		sh->hd = NULL;
+	}
 	if (sh->pipes)
 		free_pipes(sh);
 }
