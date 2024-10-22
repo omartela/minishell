61e720813cb14048380f5b5ff4591b175afcd0e2
Rychkov Iurii <irychkov@student.hive.fi>
2024-10-22 13:34:50 +0300

    draft for show_syntax_error

--- src/sys_errors.c
+++ src/sys_errors.c
@@ -6,7 +6,7 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 11:02:11 by irychkov          #+#    #+#             */
-/*   Updated: 2024/10/22 11:09:27 by irychkov         ###   ########.fr       */
+/*   Updated: 2024/10/22 13:05:09 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,4 +28,11 @@ void	error_execve_and_permission(t_shell *sh, t_cmd *cmd, int is_abs_relative)
 	check_permissions(sh, cmd, is_abs_relative);
 	error_sys("execve failed\n");
 	exit_and_free(sh, cmd, 1);
+}
+
+void	show_syntax_error(char *element)
+{
+	error_sys("minishell: syntax error near unexpected token `");
+	write(2, element, ft_strlen(element));
+	write(2, "'\n", 2);
 }
