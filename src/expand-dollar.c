718c231e3665936d4904da17ba22fa469a2e1b50
Oskari Martela <omartela@c1r2p4.hive.fi>
2024-10-22 15:28:17 +0300

    Progressing... expand dollar norminette...

--- src/expand-dollar.c
+++ src/expand-dollar.c
@@ -69,25 +69,37 @@ char    *append_characters(char *result, char *str, int i)
     return (result);
 }
 
+int var_name_len(char *str, int *i)
+{
+    int key_len;
+
+    key_len = 0;
+    while (ft_isalnum(str[*i + key_len]) || str[*i + key_len] == '_') // Get the length of the variable name
+        key_len++;
+    return (key_len);
+}
+
+char    *extract_key_and_expand(t_shell *sh, char *str, int *i, int key_len)
+{
+    char    *key;
+    char    *insert;
+
+    key = ft_substr(str, *i, key_len); // Extract the variable name
+    if (!key)
+        return (NULL);
+    insert = expand(sh->envp, key); // Expand the variable
+    free(key);
+    return (insert);
+}
 char    *handle_dollarvarname(t_shell *sh, char *result, char *str, int *i)
 {
     int     key_len;
-    char    *key;
     char    *insert;
     char    *temp;
 
     *i += 1;
-    key_len = 0;
-    while (ft_isalnum(str[*i + key_len]) || str[*i + key_len] == '_') // Get the length of the variable name
-        key_len++;
-    key = ft_substr(str, *i, key_len); // Extract the variable name
-    if (!key)
-    {
-        free(result);
-        return (NULL);
-    }
-    insert = expand(sh->envp, key); // Expand the variable
-    free(key);
+    key_len = var_name_len(str, i);
+    insert = extract_key_and_expand(sh, str, i, key_len);
     if (!insert)
     {
         free(result);
@@ -188,8 +200,6 @@ char    *handle_dollar(t_shell *sh, t_expand_state *state, char *str)
         if (str[state->i + 1] == '?') // Handle $? (exit code)
         {
             state->result = handle_exit_code(sh, state->result);
-            if (!state->result)
-                return (NULL);
             state->i += 2; // Skip the $ and ?
         }
         else if (ft_isdigit(str[state->i + 1]) && !state->in_double_quotes)
@@ -200,28 +210,44 @@ char    *handle_dollar(t_shell *sh, t_expand_state *state, char *str)
         else if ((str[state->i + 1] == '\'' || str[state->i + 1] == '\"') && !state->in_double_quotes)
             state->i += 1;
         else if (ft_isalpha(str[state->i + 1]) || str[state->i + 1] == '_') // Handle $VAR_NAME
-        {
             state->result = handle_dollarvarname(sh, state->result, str, &state->i);
-            if (!state->result)
-                return (NULL);
-        }
         else
-        {
             state->result = only_dollar(state);
-            if (!state->result)
-                return (NULL);
-        }
     }
     else
     {
         state->result = append_characters(state->result, str, state->i);
-        if (!state->result)
-            state->result = NULL;
         state->i += 1;
     }
     return (state->result);
 }
-
+char *parse_and_expand(t_shell *sh, t_expand_state *state, char *str)
+{
+    while (str[state->i])
+    {
+        handle_quotes(str[state->i], &state->in_single_quotes, &state->in_double_quotes);
+        if (state->i == 0 && (ft_strncmp(str, "~\0", 2) == 0  || ft_strncmp(&str[state->i], "~/", 2) == 0) && !state->in_double_quotes && !state->in_single_quotes)
+        {
+            state->result = handle_only_tilde(sh, state->result, str, state->i);
+            return (state->result);
+        }
+        if (str[state->i + 1] == '~') 
+        {
+            if ((ft_strncmp(&str[state->i], " ~ ", 3) == 0 || ft_strncmp(&str[state->i], " ~\0", 3) == 0 || ft_strncmp(&str[state->i], " ~/", 3) == 0) && !state->in_double_quotes && !state->in_single_quotes)
+            {
+                state->result = handle_tilde_middle(sh, state->result);
+                if (!state->result)
+                    return (NULL);
+                state->i += 2;
+                continue;
+            }
+        }
+        state->result = handle_dollar(sh, state, str);
+        if (!state->result)
+            return (NULL);
+    }
+    return (state->result);
+}
 char    *expand_input(char *str, t_shell *sh)
 {
     t_expand_state state;
@@ -230,29 +256,7 @@ char    *expand_input(char *str, t_shell *sh)
     state.result = ft_strdup("");
     if (!state.result)
         return (NULL);
-    while (str[state.i])
-    {
-        handle_quotes(str[state.i], &state.in_single_quotes, &state.in_double_quotes);
-        if (state.i == 0 && (ft_strncmp(str, "~\0", 2) == 0  || ft_strncmp(&str[state.i], "~/", 2) == 0) && !state.in_double_quotes && !state.in_single_quotes)
-        {
-            state.result = handle_only_tilde(sh, state.result, str, state.i);
-            return (state.result);
-        }
-        if (str[state.i + 1] == '~') 
-        {
-            if ((ft_strncmp(&str[state.i], " ~ ", 3) == 0 || ft_strncmp(&str[state.i], " ~\0", 3) == 0 || ft_strncmp(&str[state.i], " ~/", 3) == 0) && !state.in_double_quotes && !state.in_single_quotes)
-                {
-                    state.result = handle_tilde_middle(sh, state.result);
-                    if (!state.result)
-                        return (NULL);
-                    state.i += 2;
-                    continue;
-                }
-        }
-        state.result = handle_dollar(sh, &state, str);
-        if (!state.result)
-            return (NULL);
-    }
+    state.result = parse_and_expand(sh, &state, str);
     return (state.result);
 }
 
