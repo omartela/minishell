8a00dcc4b5fe83d4356c6a544dc51f152167e083
omartela <Oskari Martela>
2024-10-23 11:37:18 +0300

    Fixes norminette issues in init_env.c

--- src/init_env.c
+++ src/init_env.c
@@ -6,26 +6,53 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 11:07:15 by irychkov          #+#    #+#             */
-/*   Updated: 2024/09/08 00:40:07 by irychkov         ###   ########.fr       */
+/*   Updated: 2024/10/23 11:32:32 by omartela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-static void	alloc_tables(t_shell *sh, char ***c_envp, char ***l_shvars, size_t i)
+static void	alloc_tables(t_shell *sh, char ***c_envp, char ***l_vars, size_t i)
 {
 	*c_envp = ft_calloc(i, sizeof(char *) + 1);
-	*l_shvars = ft_calloc(i, sizeof(char *) + 1);
-	if (!c_envp || !l_shvars)
+	*l_vars = ft_calloc(i, sizeof(char *) + 1);
+	if (!c_envp || !l_vars)
 	{
 		if (c_envp)
 			free(c_envp);
-		if (l_shvars)
-			free(l_shvars);
+		if (l_vars)
+			free(l_vars);
 		sh->envp = NULL;
 		sh->local_shellvars = NULL;
 		error_sys("Copy environment failed..\n");
 		exit(1);
+	}
+}
+
+static void	copy_vars(char **envp, char **c_envp, char **l_vars, size_t sarray)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < sarray)
+	{
+		c_envp[i] = ft_strdup(envp[i]);
+		if (!c_envp[i])
+		{
+			free_array_back(c_envp, i);
+			free_array_back(l_vars, i);
+			error_sys("Copy environment failed..\n");
+			exit(1);
+		}
+		l_vars[i] = ft_strdup(envp[i]);
+		if (!l_vars[i])
+		{
+			free_array_back(l_vars, i);
+			free_array_back(c_envp, i + 1);
+			error_sys("Copy environment failed..\n");
+			exit(1);
+		}
+		++i;
 	}
 }
 
@@ -34,36 +61,14 @@ void	copy_env(char **envp, t_shell *shell)
 	size_t	sarray;
 	char	**copied_envp;
 	char	**local_shellvars;
-	size_t	i;
 
 	sarray = 0;
-	i = 0;
 	// think what to do if envp is "empty" or does not exist
 	sarray = calculate_table_size(&envp);
 	alloc_tables(shell, &copied_envp, &local_shellvars, sarray);
-	i = 0;
-	while (i < sarray)
-	{
-		copied_envp[i] = ft_strdup(envp[i]);
-		if (!copied_envp[i])
-		{
-			free_array_back(copied_envp, i);
-			free_array_back(local_shellvars, i);
-			error_sys("Copy environment failed..\n");
-			exit(1);
-		}
-		local_shellvars[i] = ft_strdup(envp[i]);
-		if (!local_shellvars[i])
-		{
-			free_array_back(local_shellvars, i);
-			free_array_back(copied_envp, i + 1);
-			error_sys("Copy environment failed..\n");
-			exit(1);
-		}
-		++i;
-	}
-	copied_envp[i] = NULL;
-	local_shellvars[i] = NULL;
+	copy_vars(envp, copied_envp, local_shellvars, sarray);
+	copied_envp[sarray] = NULL;
+	local_shellvars[sarray] = NULL;
 	local_shellvars = sort_table(local_shellvars);
 	shell->envp = copied_envp;
 	shell->local_shellvars = local_shellvars;
