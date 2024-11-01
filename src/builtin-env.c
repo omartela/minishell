e74d421e67c7931e1b11103b0fee2cdda795235a
Rychkov Iurii <irychkov@student.hive.fi>
2024-11-01 15:00:18 +0200

    Added result of test and fixed env args and options

--- src/builtin-env.c
+++ src/builtin-env.c
@@ -6,7 +6,7 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 17:40:47 by omartela          #+#    #+#             */
-/*   Updated: 2024/10/08 17:11:33 by irychkov         ###   ########.fr       */
+/*   Updated: 2024/11/01 14:58:28 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,10 @@ int	env(t_shell *shell, char **args)
 			ft_printf("%s\n", shell->envp[i]);
 			++i;
 		}
+		return (0);
 	}
-	return (0);
+	/* return (0); */
+	else
+		/* return (show_error_return(127, args[0], "no options or arguments!")); */
+		return 0;
 }
